374e989017b9b07dd1c034792b1a2824a18f839e
James Hartman <surfsnowpro@gmail.com>
2021-11-22 07:49:40 -0700

    update landing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          What it is?!
+          What it is, sucka?!
         </p>
         <a
           className="App-link"
